c0b4eb94b5ede81bb290cd7f62170ea4efd201a0
Edriell <edritheone@gmail.com>
2024-02-19 15:02:06 -0400

    6-size.c fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-sizeof(char);
-sizeof(int);
-sizeof(long int);
-sizeof(long long int);
-sizeof(float);
+sizeof(char));
+sizeof(int));
+sizeof(long int));
+sizeof(long long int));
+sizeof(float));
 return (0);
 }
