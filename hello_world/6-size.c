4a61bb437adc9ff8826fea2c1193579126388bb1
Edriell <edritheone@gmail.com>
2024-02-19 14:49:15 -0400

    Adding 6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+sizeof (c);
+sizeof (int);
+sizeof (long);
+sizeof (long long);
+sizeof (float);
+return (o);
+}
