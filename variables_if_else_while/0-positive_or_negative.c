d59b17ba7e0c1c5ff9a66dd710ef62ab2450d4c6
Edriell <edritheone@gmail.com>
2024-02-20 11:29:25 -0400

    Adding 0-positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
@@ -13,15 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	{
+	printf("%d ", n);
+	}
 	if (n > 0)
 	{
 	   printf("n is positive\n");
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 	printf("n is zero\n");
 	}
-	if (n < 0)
+	else (n < 0)
 	{
 	printf("n is negative\n");
 	}
