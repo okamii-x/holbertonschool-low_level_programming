602b22b76c40ec4d901f53fc5a1185a699fbcfe3
Edriell <edritheone@gmail.com>
2024-02-21 10:39:00 -0400

    Adding 0-_putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * main- Entry point
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char c = "_putchar\n")
+
+	{
+	_putchar (c);
+	}
+	return (0);
+}
