c42e8b53b53a3e9702561d6dd5ce1f2dda6c5746
Edriell <edritheone@gmail.com>
2024-02-21 12:14:18 -0400

    Adding 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include <main.h>
-#include <unistd.h>
-
+#include "main.h"
 /**
- * main- Entry point
+ * main- Ptints _putchar, followed by a new line.
  *
  * Return Always 0 (Success)
  */
-
 int main(void)
 {
-	for (char c = "_putchar\n")
-
-	{
-	_putchar (c);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
