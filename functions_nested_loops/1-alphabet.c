8c61af890ca801423e3061230cc222a6dd0dd91b
Edriell <edritheone@gmail.com>
2024-02-21 13:40:28 -0400

    Adding 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *main- Prints the alphabet, in lowercase, followed by a new line
+ *
+ *Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++;)
+	{
+	_putchar(e);
+	}
+	_putchar('\n')
+}
